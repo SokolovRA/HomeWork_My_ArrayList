4e70e64466236e8f969672fac2c3bf031bb01533
zero77787 <rsokolov995@gmail.com>
2023-02-15 22:45:51 +0300

    Home Work (done)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import stringList.StringListImpl;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        StringListImpl stringList = new StringListImpl(10);
+        stringList.add("1");
+        stringList.add("2");
+        stringList.add("3");
+        stringList.add("4");
+        stringList.add("5");
+        stringList.add("6");
+        stringList.add("7");
+//        stringList.add(9,"5");
+//        stringList.set(1,"7");
+//        stringList.remove("8");
+//        stringList.remove(2);
+//        stringList.indexOf("2");
+//        System.out.println(stringList.contains("2"));
+//        System.out.println(stringList.indexOf("1"));
+//        System.out.println(stringList.lastIndexOf("3"));
+//        System.out.println(stringList.get(2));
+//        stringList.clear();
+//        stringList.toArray();
+        System.out.println(stringList);
 
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(2);
+        System.out.println(stringList.equals(list));
     }
 }
